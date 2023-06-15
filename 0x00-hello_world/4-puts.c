e190636672188a37419d4edc3c7424be49bf916b
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-15 16:51:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 int puts(const char *str);
 
 #include<stdio.h>
-int main(void)
+int main(void) /*Prints a string statement*/
 {
 	char answer[] = "\"Programming is like building a multilingual puzzle";
 
