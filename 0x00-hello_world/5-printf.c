1fe329352ccced1a7cab6fdc406b42c0e887b70e
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-15 18:12:41 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char answer[] = "with proper grammar, but the outcome is a piece of art,"
+
+	printf(answer);
+
+	return (0);
+}
