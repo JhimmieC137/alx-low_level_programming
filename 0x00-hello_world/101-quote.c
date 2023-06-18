4b2590a485b00d052735ca9d5963d06af692a8cb
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-18 14:19:04 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: 'Using omething other than putchar'
+ * Description: 'Using something other than putchar'
  *
  * Return: 1 (Success)
  *
@@ -11,7 +13,7 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 
 	return (1);
 }
