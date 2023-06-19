089b0af028cf9e03971d7bbd5b648cabc6a25902
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 16:41:44 -0700

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,19 @@
 
 int main(void)
 {
-	putchar('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+	char lower[26] = "abcdefghijklmnopqrstuvwxyz";
+	char upper[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
+	for (i = 0; i < 26; i++)
+	{
+		putchar(lower[i]);
+	}
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(upper[i]);
+	}
+	putchar('\n');
 	return (0);
 }
