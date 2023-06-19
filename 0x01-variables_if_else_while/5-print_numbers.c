baca908071d71d79645d22a44b7d40076fdc1f48
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 16:58:33 -0700

    Alphabet song

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char number[10] = "0123456789";
+	
+	int i;
+
+	for (i = 0; i < 10)
+	{
+		putchar(number[i]);
+	}
+	putchar('\n');
+	return (0);
+}
