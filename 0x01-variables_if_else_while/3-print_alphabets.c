30a1b3dca58ba3a4ca0872d53a11daa65dbddbe0
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 16:34:53 -0700

    Print alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printing alphabets in upper and lower case
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	putchar('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+	return (0);
+}
