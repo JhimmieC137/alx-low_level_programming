ed26af3492bed4143c4ea669995d85bdf422bb8b
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 14:50:09 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printing Alphabets
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	alpha[26] = "abcdefghijklmnopqrstuvwxyz\n";
+
+	putchar(alpha);
+
+	return (0);
+}
