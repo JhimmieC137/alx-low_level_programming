28ad69d3021f98ac9d1f85f864f9d61621110ef0
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 19:25:43 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,9 +13,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar("%d, ", i);
+		putchar(i);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar('');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
