1baaecfe023c42f0330896eea781227138b7281e
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 19:42:01 -0700

    Combo3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing combinations of numbers
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i, x, y;
+
+	for (i = 48; i <  58; i++ )
+	{
+		for (x = 49; x < 58; x++)
+		{
+			for (y = 50; y < 58; y++)
+			{
+				if (y > x && x > i)
+				{
+					putchar(i);
+					putchar(x);
+					putchar(y);
+
+					if (n != 55 || m !=56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
