cdb05b590343fc55304574a15ac0718f713fd14b
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 18:21:56 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Level
+ *
+ * Description: Hexadecimals
+ *
+ * Returd: Always 0 (Sussess)
+ *
+ */
+
+int main(void)
+{
+	char hexa[15] = "0123456789abcdef"
+	
+	int i;
+
+	for (i==0; i < 15; i++)
+	{
+		putchar(hexa[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
