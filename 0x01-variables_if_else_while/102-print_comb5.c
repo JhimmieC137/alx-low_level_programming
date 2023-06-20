0511e6c71b8499f83f847cd01accd0467091e3c6
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 21:47:54 -0700

    combine

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,12 +26,10 @@ int main(void)
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
-					
-                                        putchar(' ');
-				
+					putchar(' ');
 				}
 
-                        }
+			}
 
 		}
 	}
