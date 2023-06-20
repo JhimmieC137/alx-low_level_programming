bed4cbfd557ec15c2cda1bee79a35e8dd28f6499
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 19:17:35 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Going far
+ *
+ * Return: Alwasy 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar("%d, ", i);
+	}
+	return (0);
+}
