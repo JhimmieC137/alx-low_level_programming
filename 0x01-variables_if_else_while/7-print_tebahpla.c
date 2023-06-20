1c296ef511a3f9971e2ac3c30f96e0fcaaaabbf2
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-19 17:56:47 -0700

    Print number

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printing in reverse/
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char alpha[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
+	
