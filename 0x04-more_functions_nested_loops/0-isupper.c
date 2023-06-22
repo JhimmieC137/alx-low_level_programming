55f53df5b1e8368d2797014b32cc4d74814d7c38
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-22 16:05:48 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ * Description: Detecting uppercase letters
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(char c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
