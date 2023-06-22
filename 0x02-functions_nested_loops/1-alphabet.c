7a9ced4faa78783743a5924b513459eb79903398
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-21 17:21:56 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Display the alphabets
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(c);
+	}
+	_puchar('\n');
+}
