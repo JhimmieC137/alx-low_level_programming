0e543376df2d29b88200f84a756319a3e969765b
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-22 21:29:25 -0700

    print_mosdt_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  Entry point
+ *
+ * Description: What the title says
+ *
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (!(x == '2' || x == '4'))
+			_putchar(x);
+	}
+	_putchar('\n');
+}
