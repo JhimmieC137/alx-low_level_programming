b3d815ba189bd4887125250a67ad3b259652fd7d
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-22 22:03:33 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,16 @@
 
 void more_numbers(void)
 {
-	int i, j;
-
-	for  (i = 0; j <= 14; j++)
+	int i, x;
+	
+	for  (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (x = 0; x < 14; x++)
 		{
-			if (j >= 10)
-			{
-				_putchar(1);
-				_putchar(j % 10 + '0');
-			}
-			_putchar('\n');
+			if (x >= 10)
+			_putchar('1');
+			_putchar(x % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }
