0dbf3e30454be675b5fc8b68e3c290d2287c6fdd
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-22 21:04:19 -0700

    mulk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: Multiplying numbers
+ *
+ * @a: Integer one
+ * @b: Integer two
+ *
+ * Return: Answer digit
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
