80f1806f696037e7770a4577dbb5f1f405eb5840
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-26 10:15:25 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,13 +17,13 @@ void print_rev(char *s)
 	{
 		d++;
 	}
+
 	d--;
+
 	while (d >= 0)
 	{
-		/*if (*(s + d) != '\0')
-		{*/
-			_putchar(*(s + d));
-			d--;
+		_putchar(*(s + d));
+		d--;
 	}
 	_putchar('\n');
 }
