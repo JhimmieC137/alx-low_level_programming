db5e7abb39395030eb6ed88e3b7d32580a163e0c
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-26 14:55:16 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,8 +14,8 @@ void puts2(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		if ((i + 1) %  2 == 0)
-			_putchar(*(str + (i - 1)));
+		if ((i + 1) %  2 != 0)
+			_putchar(*(str + i));
 		i++;
 	}
 	_putchar('\n');
