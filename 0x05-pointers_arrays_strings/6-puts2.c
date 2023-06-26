a64e1844b31f424b32082dd67db162f11d913e0f
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-26 12:41:00 -0700

    print other numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other number
+ * starting with the first
+ *
+ * @str: pointer argument
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 1;
+
+	while (*(str + i) != '\0')
+	{
+		if ((i + 1) %  2 == 0)
+			_putchar(*(str + (i - 1)));
+		i++;
+	}
+	_putchar('\n');
+}
