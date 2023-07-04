cc9f9abe9c62b68898284bc44c1de407fd520d86
JhimmieC137 <toluwalope.david@gmail.com>
2023-07-04 10:38:38 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - Searches for substrings within strings
+ *
+ * @haystack: Main string
+ * @needle: Substring
+ *
+ * Return: pointer to string location in main string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int x = 0;
+	int y = 0;
+	int s;
+
+	while (needle[x] != '\0')
+		x++;
+
+	while (haystack[y] != '\0')
+	{
+		for (s = 0; s <= x; s++)
+		{
+			if (haystack[(y + s)] == needle[s])
+			{
+				if (s == x)
+					return (&haystack[y]);
+			}
+			else
+				break;
+		}
+		y++;
+	}
+	return (NULL);
+}
