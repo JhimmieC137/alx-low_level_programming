75192e522c9d21fc6742cb85378c126b3f778710
JhimmieC137 <toluwalope.david@gmail.com>
2023-07-04 08:45:09 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - finds the first occurrance of a
+ * character in a string
+ *
+ * @s: String pointer
+ * @c: character to be found
+ * Return: Pointer or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+
+	while (*(s + x) != '\0')
+	{
+		if (*(s + x) == c)
+		{
+			return (s + x);
+		}
+		x++;
+	}
+	return ("NULL");
+}
