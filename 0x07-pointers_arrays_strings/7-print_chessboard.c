df0198deec97e2d7235cf984eb17ef44936fb2e4
JhimmieC137 <toluwalope.david@gmail.com>
2023-07-04 14:37:50 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -16,11 +16,11 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 
-		for (j = 0; j <= 8; j++)
+		for (j = 0; j < 8; j++)
 		{
-			if (j == 8)
-				_putchar('\n');
 			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
 		}
 	}
 }
