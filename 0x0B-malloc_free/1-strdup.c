0360a925dba20ad24eaf2aa09c481e3d4a8bfedf
JhimmieC137 <toluwalope.david@gmail.com>
2023-07-12 02:40:06 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Copies string to different variable
+ *
+ * @str: String pointer
+ *
+ * Return: pointer to new string
+ */
+
+char *_strdup(char *str)
+{
+	char *b;
+	int x = 0;
+	int y;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[x] != '\0')
+		x++;
+
+	b = (char *) malloc(x * sizeof(char));
+
+	if (b == NULL)
+		return (NULL);
+
+	for (y = 0; y < x; y++)
+		b[y] = str[y];
+
+	return (b);
+
+}
