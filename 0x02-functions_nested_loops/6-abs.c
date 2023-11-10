afcf021b8ef97e723ee32a34000e6615d2058230
JhimmieC137 <toluwalope.david@gmail.com>
2023-11-10 08:49:03 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value
+ *
+ * @n: integer
+ *
+ * Return: integear
+ *
+ */
+
+int _abs(int n)
+{
+	int y = -1;
+
+	if (n < 0)
+		return (n * y);
+	return (n);
+}
